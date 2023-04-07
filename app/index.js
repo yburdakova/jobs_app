772f2f5c671c8d066279cb78beb10e5465fb3e6a
Yana Burdakova <bu.asti@gmail.com>
2023-04-07 18:13:18 -0400

    First commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import { Link, Stack } from "expo-router";
+
+import React from 'react';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>
+                Yana`s application`
+            </Text>
+        </View>
+    );
+};
+
+export default Home;
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/yana.jpg";
+
+export default {
+  profile,
+};
